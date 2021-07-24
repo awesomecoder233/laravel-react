419d2b485d0bd4cb062104ed5149f4f0b1a943f8
awesomecoder233 <tommount58@gmail.com>
2021-07-24 10:52:43 +0200

    complete product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,47 @@ use App\Product;
 
 class ProductController extends Controller
 {
-    //
+    //Display a listing of the resource
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+    
+    //store a newly created resource in storage
+    public function store(Request $request)
+    {
+        $product = new Product([
+            'title' => $request->get('title'),
+            'body' => $request->get('body')
+        ]);
+        $product->save();
+        return response()->json('product Added Successfully');
+    }
+
+    //show the form for editing the specified resource
+    public function edit($id)
+    {
+        $product = Product::find($id);
+        return response()->json($product);
+    }
+    
+    //update the specified resource in storage
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->title = $request->get('title');
+        $prodyuct->body = $request->get('body');
+        $product->save();
+
+        return response()->json('Product Update Successfully');
+    }
+
+    //Remove the specified resource from storage
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+        return response()->json('Product deleted successfully');
+    }
 }
